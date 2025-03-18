ebdfd9953fd983d09e76499015441683e39c0b75
savan777 <quesman01@gmail.com>
2025-03-18 12:18:32 +0900

    ✨ お試し修正

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Intro from './Intro.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Intro/>
   </StrictMode>,
 )
